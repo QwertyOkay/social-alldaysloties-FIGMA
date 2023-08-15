bc36821764a2adb4c0335ec033b3de62887779f7
Alexandr Kotenko <kpshmt@gmail.com>
2023-08-15 13:27:01 +0300

    update

---
+++ src/components/footerModals/PrivacyModal.jsx
@@ -0,0 +1,131 @@
+import Modal from 'components/modal';
+import ButtonClose from 'components/buttonClose/ButtonClose';
+import ModalTextWrap from 'components/modalTextWrap/ModalTextWrap';
+
+function PrivacyModal({ showPrivacy }) {
+  return (
+    <>
+      {showPrivacy && (
+        <Modal variant="modal" closeModal={showPrivacy}>
+          <ButtonClose
+            variant="closeBtnLight"
+            aria-label={'close'}
+            type={'button'}
+            onClick={() => showPrivacy(false)}
+          />
+          <ModalTextWrap title={'Privacy Policy'}>
+            <p>Introduction</p>
+            <p>
+              This Privacy Policy describes how Royal Coinage ("we," "us," or
+              "our") collects, uses, and protects the personal information of
+              users ("you" or "your") when you visit or interact with our
+              website Alldaysloties.com ("Website"). We are committed to
+              safeguarding your privacy and ensuring the security of your
+              personal information. By using our Website, you consent to the
+              collection and use of your personal information as outlined in
+              this Privacy Policy.
+            </p>
+            <p>Information We Collect</p>
+            <p>
+              Personal Information: We may collect personal information that you
+              voluntarily provide to us when you interact with our Website, such
+              as your name, email address and any other information you choose
+              to provide. Usage Information: We may automatically collect
+              certain information about your visit to our Website, including
+              your IP address, browser type, operating system, referring URLs,
+              pages visited, and the date and time of your visit. This
+              information helps us analyze trends, administer the Website, track
+              user movements, and gather demographic informatio
+            </p>
+            <p>Use of Information</p>
+            <p>
+              Personal Information: We may use the personal information you
+              provide to us for the following purposes:
+            </p>
+            <ul>
+              <li>
+                To provide and maintain our services, including responding to
+                your inquiries and fulfilling your requests.
+              </li>
+              <li>
+                To personalize your experience on our Website and improve user
+                experience.
+              </li>
+              <li>
+                To send you promotional materials, newsletters, and other
+                communications related to our services.
+              </li>
+              <li>
+                To process transactions and complete orders for products or
+                services you purchase from us.
+              </li>
+              <li>
+                To comply with legal obligations, resolve disputes, and enforce
+                our agreements.
+              </li>
+            </ul>
+            <p>
+              Usage Information: We may use the usage information collected to
+              analyze and improve the functionality and performance of our
+              Website, identify user preferences, and enhance user experience.
+            </p>
+            <p>Information Sharing</p>
+            <p>
+              Third-Party Service Providers: We may share your personal
+              information with trusted third-party service providers who assist
+              us in operating our Website, conducting our business, or providing
+              services to you. These service providers have access to your
+              personal information only as necessary to perform their functions
+              and are obligated to maintain its confidentiality. Legal
+              Compliance: We may disclose your personal information if required
+              by law, regulation, legal process, or governmental request, or to
+              protect our rights, property, or safety, as well as the rights,
+              property, or safety of our users or others.
+            </p>
+            <p>Data Security</p>
+            <p>
+              We implement reasonable security measures to protect your personal
+              information from unauthorized access, alteration, disclosure, or
+              destruction. However, no method of data transmission over the
+              internet or electronic storage is completely secure, and we cannot
+              guarantee absolute security of your information.
+            </p>
+            <p>Links to Third-Party Websites</p>
+            <p>
+              Our Website may contain links to third-party websites. We have no
+              control over the content, privacy policies, or practices of these
+              websites. We encourage you to review the privacy policies of any
+              third-party websites you visit.
+            </p>
+            <p>Children's Privacy</p>
+            <p>
+              Our Website is not intended for individuals under the age of 18.
+              We do not knowingly collect or solicit personal information from
+              anyone under the age of 18. If we become aware that we have
+              collected personal information from a child under the age of 18,
+              we will promptly delete it. If you believe that we have any
+              information from or about a child under the age of 18, please
+              contact us using the provided contact details.
+            </p>
+            <p>Changes to this Privacy Policy</p>
+            <p>
+              We reserve the right to update or modify this Privacy Policy at
+              any time. Any changes will be effective immediately upon posting
+              the revised Privacy Policy on our Website. We encourage you to
+              review this Privacy Policy periodically for any updates or
+              changes.
+            </p>
+            <p>Contact Us</p>
+            <p>
+              If you have any questions, comments, or concerns regarding this
+              Privacy Policy or our privacy practices, please contact us at
+              supportteam@Alldaysloties.com.
+            </p>
+          </ModalTextWrap>
+        </Modal>
+      )}
+    </>
+  );
+}
+
+export default PrivacyModal;
---
+++ src/components/footerModals/TermsModal.jsx
@@ -0,0 +1,122 @@
+import Modal from 'components/modal';
+import ButtonClose from 'components/buttonClose/ButtonClose';
+import ModalTextWrap from 'components/modalTextWrap/ModalTextWrap';
+
+function TermsModal({ showTerms }) {
+  return (
+    <>
+      {showTerms && (
+        <Modal variant="modal" closeModal={showTerms}>
+          <ButtonClose
+            aria-label={'close'}
+            type={'button'}
+            onClick={() => showTerms(false)}
+          />
+          <ModalTextWrap title={'Terms & Conditions'}>
+            <p>
+              Please read these Terms and Conditions ("Agreement") carefully
+              before using the Alldaysloties.com website ("Website"). This
+              Agreement sets forth the legally binding terms and conditions for
+              your use of the Website. By accessing or using the Website, you
+              agree to be bound by this Agreement.
+            </p>
+            <p>Intellectual Property</p>
+            <p>
+              Ownership: The Website and its original content, features, and
+              functionality are owned by Royal Coinage and are protected by
+              international copyright, trademark, patent, trade secret, and
+              other intellectual property or proprietary rights laws. Limited
+              License: Subject to your compliance with this Agreement, we grant
+              you a limited, non-exclusive, non-transferable, and revocable
+              license to access and use the Website for personal, non-commercial
+              purposes. Restrictions: You agree not to reproduce, distribute,
+              modify, create derivative works of, publicly display, publicly
+              perform, republish, download, store, or transmit any of the
+              material on our Website, except as permitted by this Agreement.
+            </p>
+            <p>User Responsibilities</p>
+            <p>
+              Prohibited Conduct: You agree not to engage in any conduct that
+              violates applicable laws, infringes upon the rights of others, or
+              interferes with the operation or security of the Website.
+              Prohibited conduct includes, but is not limited to:
+            </p>
+            <p>
+              Uploading or transmitting any unauthorized or harmful content.
+              Attempting to gain unauthorized access to the Website or its
+              servers. Interfering with or disrupting the Website or its
+              networks. Engaging in any activity that could damage, disable,
+              overburden, or impair the Website.
+            </p>
+            <p>
+              Account Registration: In order to access certain features of the
+              Website, you may be required to create an account. You are
+              responsible for maintaining the confidentiality of your account
+              information and for all activities that occur under your account.
+            </p>
+            <p>
+              User Submissions: If you submit any content, feedback, or
+              suggestions to us through our Website, you grant us a
+              non-exclusive, royalty-free, perpetual, and irrevocable right to
+              use, reproduce, modify, adapt, publish, translate, distribute, and
+              display such submissions in any form or medium.
+            </p>
+            <p>Disclaimer of Warranty</p>
+            <p>
+              The Website is provided on an "as-is" and "as available" basis. We
+              make no representations or warranties of any kind, express or
+              implied, regarding the operation, availability, accuracy,
+              reliability, or suitability of the Website. To the fullest extent
+              permitted by law, we disclaim all warranties, including but not
+              limited to, implied warranties of merchantability and fitness for
+              a particular purpose.
+            </p>
+
+            <p>Limitation of Liability</p>
+            <p>
+              To the maximum extent permitted by law, Royal Coinage and its
+              affiliates, officers, directors, employees, agents, and partners
+              shall not be liable for any direct, indirect, incidental,
+              consequential, or special damages arising out of or in any way
+              connected with your use of the Website or the content contained
+              therein. This limitation of liability applies to all claims,
+              whether based on contract, tort, negligence, strict liability, or
+              any other legal theory.
+            </p>
+            <p>Modifications</p>
+            <p>
+              We reserve the right to modify or replace this Agreement at any
+              time. Any changes will be effective immediately upon posting the
+              updated Agreement on the Website. Your continued use of the
+              Website after any such changes constitutes your acceptance of the
+              modified terms.
+            </p>
+            <p>Severability</p>
+            <p>
+              If any provision of this Agreement is found to be invalid or
+              unenforceable, the remaining provisions shall be enforced to the
+              fullest extent permitted by law, and the unenforceable provision
+              shall be deemed modified to the extent necessary to make it
+              enforceable.
+            </p>
+            <p>Entire Agreement</p>
+            <p>
+              This Agreement constitutes the entire agreement between you and
+              Royal Coinage regarding your use of the Website and supersedes all
+              prior and contemporaneous agreements, representations, and
+              understandings.
+            </p>
+            <p>Contact Us</p>
+            <p>
+              If you have any questions, comments, or concerns regarding these
+              Terms and Conditions, please contact us at
+              supportteam@Alldaysloties.com.
+            </p>
+          </ModalTextWrap>
+        </Modal>
+      )}
+    </>
+  );
+}
+
+export default TermsModal;
